87636295fcd9adb5978ba564046cbed76ca63764
SpellingBee278 <49443652+spellingbee278@users.noreply.github.com>
2020-06-09 17:26:04 +0100

    made readme.md to do what to do

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # learn-python
-I'm learning Python
+
+First to start this python script in learn-python/python/main.py, you have to do this in your IDE's terminal below (this only works for python 3.8):
+
+cd learn-python
+
+Then you have to do:
+
+cd python
+
+Finally do:
+
+python main.py
+
--- python/main.py
+++ python/main.py
@@ -1,10 +1,53 @@
-# print("Hello world")
+print("Welcome to Ilyas' programmed system")
+as_well = "Me too!"
+name = input("What is your name? ")
+age = input("What is your age? ")
+school = input("What is/was your school name? ")
+langs = ["HTML", "CSS", "JS"]
 
-animal = "An animal"
+if school == "Lowerplace":
+    print("You are my friend :)")
+else:
+    print("Ok")
+    if school == "lowerplace":
+        print("You are my friend :)")
 
-animalsILike = ["Chicks", "chicks"]
 
-print(animal)
-   animal_question = input("Which animal do you like? ")
+coding_question = input("Do you code? ")
+if coding_question == "yes":
+                print(as_well)
+
+else:
+    print("Ok")
+if coding_question == "Yes":
+    print(as_well)
+
+else:
+    print("You can learn how to code by doing youtube tutorials")
+if coding_question == "yes":
+    print(as_well)
+
+else:
+    print("You can learn how to code by doing youtube tutorials")
+
+pl_code = input("What programming language(s) do you code? ")
+if pl_code in langs:
+            print("Wow, I code ", pl_code , "too!")
+else:
+    print("Wow! You are more experienced than me")
+
+h_school = input("What was/is/which highschool did you go/are you in/planning to go to? ")
+if h_school == "Oulder Hill":
+    print(as_well)
+else:
+    print("Ok")
+
+if coding_question == "Yes":
+    yrs_of_code = input("How long have you been coding for?")
 
 
+
+print()
+s_ended = "         ---Survey Ended---          "
+print(s_ended)
+print()
