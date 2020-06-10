ec63375ba514c631bc3e61d905c0ebfc18b1a3a2
Waseem Aslam <w.aslam@rocketmail.com>
2020-06-10 15:39:33 +0100

    changes to animal.py

--- python/animal.py
+++ python/animal.py
@@ -1,4 +1,24 @@
+# variables
 animal = input("What animal do you like? ")
 animal_array = ["Chicks", "chicks"]
+chicks_array = ["Yes", "yes"]
+other_animals = [""]
+yes_answer = ["Yes", "yes"]
+# if statements
 if animal in animal_array:
     print("Me too!")
+    chicks = input("Do you have a chick? ")
+
+else:
+    print("Ok")
+
+if chicks in chicks_array:
+    chick_name = input("What is your chick's name? ")
+    chicks_age = input("What is your chick's age? ")
+
+
+else:
+  print("Ended")
+
+
+
