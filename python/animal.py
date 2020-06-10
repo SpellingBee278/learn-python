21dffae217c9a68d26aa944b6c852763297541b2
Waseem Aslam <w.aslam@rocketmail.com>
2020-06-10 21:39:01 +0100

    code dont work

--- python/animal.py
+++ python/animal.py
@@ -1,24 +1,28 @@
 # variables
+chicks = "Do you have chick?"
 animal = input("What animal do you like? ")
 animal_array = ["Chicks", "chicks"]
 chicks_array = ["Yes", "yes"]
 other_animals = [""]
 yes_answer = ["Yes", "yes"]
 # if statements
-if animal in animal_array:
+print(animal)
+
+if animal in chicks_array:
     print("Me too!")
-    chicks = input("Do you have a chick? ")
+    print(chicks + input(" "))
 
 else:
     print("Ok")
 
-if chicks in chicks_array:
+
+if chicks == yes_answer:
     chick_name = input("What is your chick's name? ")
     chicks_age = input("What is your chick's age? ")
 
 
 else:
-  print("Ended")
+    print("Ended")
 
 
 
