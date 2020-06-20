36e0847ce37d4ae4a24136ee1caaef0e8e0513b2
Waseem Aslam <w.aslam@rocketmail.com>
2020-06-20 14:49:04 +0100

    learning flask

--- python/learn-flask/tut.py
+++ python/learn-flask/tut.py
@@ -1,12 +1,15 @@
-# import the Flask web framework
-from flask import Flask, redirect, url_for
+
+# import flask framework, this tutorial series was by techwithtim
+
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
+FLASK_DEBUG = 1
 
 
-@app.route("/")
-def home():
-    return "Hello! This is the main page! <h1>HELLO!</h1>"
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content=["Stop", "joe", "Baquaaas"])
 
 
 if __name__ == "__main__":
