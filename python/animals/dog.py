e8790a8445e07494360401d1af54102005b23361
Waseem Aslam <w.aslam@rocketmail.com>
2020-07-14 18:03:01 +0100

    more code in learning classes

--- python/animals/dog.py
+++ python/animals/dog.py
@@ -6,5 +6,8 @@ class Dog(Animal):
     print("Bark")
 
   def how_do_i_move(self):
-    print("I move by running")
+    print("If I smell or see something tasty, I run to it and munch it and if I don't smell or see something tasty I "
+          "walk regularly ")
 
+  def how_much_do_i_weigh(self):
+    print("I weigh approximately 40-55lbs")
