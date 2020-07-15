4c03a49205c6f0af1eb98db7a3f3d6565ccd35f9
Waseem Aslam <w.aslam@rocketmail.com>
2020-07-15 16:14:14 +0100

    Changed zoo.py and made a bird animal

---
+++ python/animals/bird.py
@@ -0,0 +1,16 @@
+from animal import Animal
+
+
+
+class Bird(Animal):
+  def say_hello(self):
+    print("Tweet Tweet")
+
+  def get_number_of_legs(self):
+    print("I have 2 legs")
+
+  def how_do_i_move(self):
+    print("I fly in the sky and then I walk on an object (ground, shed etc.)")
+
+  def how_much_do_i_weigh(self):
+    print("I weigh 229-344 lbs")
--- python/animals/zoo.py
+++ python/animals/zoo.py
@@ -1,6 +1,7 @@
 from dog import Dog
 from cat import Cat
 from duck import Duck
+from bird import Bird
 
 What_animal = input("What animal do you want us to give information about? ")
 if What_animal == "Dog":
@@ -23,3 +24,10 @@ if What_animal == "Duck":
     d.get_number_of_legs()
     d.how_do_i_move()
     d.how_much_do_i_weigh()
+
+if What_animal == "Bird":
+    b = Bird(2)
+    b.say_hello()
+    b.get_number_of_legs()
+    b.how_do_i_move()
+    b.how_much_do_i_weigh()
